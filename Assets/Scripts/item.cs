c05b8d8a8cd95cc6e713284baa989705c7b49e76
memo <alexis45645@gmail.com>
2022-10-30 14:13:49 -0400

    added controller and item scriptabeObj

---
+++ Assets/Scripts/item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : ScriptableObject
+{
+    public string itemName;
+    public bool isMove;
+    public int hasEffect;
+    public float effectDuration;
+}
